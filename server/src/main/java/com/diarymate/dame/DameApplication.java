b2674861a824b52064ae119ca772cc26b5e5c5b8
hoosa <janghoosa@gmail.com>
2023-05-02 01:45:53 +0900

    Init Commit

---
+++ server/src/main/java/com/diarymate/dame/DameApplication.java
@@ -0,0 +1,13 @@
+package com.diarymate.dame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DameApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DameApplication.class, args);
+  }
+
+}
