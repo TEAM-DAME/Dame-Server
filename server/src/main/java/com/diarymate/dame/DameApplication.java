833c3738aa3b6d5b7638e67253f935007a54afb5
hoosa <janghoosa@gmail.com>
2023-05-13 20:18:45 +0900

    feat: 초기 설정 추가 (DAME-4)

--- server/src/main/java/com/diarymate/dame/DameApplication.java
+++ server/src/main/java/com/diarymate/dame/DameApplication.java
@@ -1,7 +1,9 @@
 package com.diarymate.dame;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DameApplication {
@@ -10,4 +12,8 @@ public class DameApplication {
     SpringApplication.run(DameApplication.class, args);
   }
 
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
 }
