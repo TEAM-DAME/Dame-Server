e15bc994b2a35b45d28b7eaf66357ae450c08226
hoosa <janghoosa@gmail.com>
2023-07-24 20:14:12 +0900

    Init Commit

---
+++ src/test/java/com/diarymate/dame/DameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diarymate.dame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DameApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
